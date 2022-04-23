46cf20494eb6ffe40b509fd6307aaae5c1fe15f1
rohit1101 <srohit1101@gmail.com>
2022-04-23 14:59:51 +0530

    add styled component plugin

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 function Layout(props) {
   return (
