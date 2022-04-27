dd38ca49975cbe840f4188c0e82927c9b6ac2835
Rohit <rohit@Rohits-MacBook-Air.local>
2022-04-27 21:24:11 +0530

    render blog using markdown

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Layout from '../../components/Layout/Layout'
+
+function BlogPost(props) {
+  return (
+    <Layout>
+        <h1>Blogs coming soon...</h1>
+    </Layout>
+  )
+}
+
+BlogPost.propTypes = {}
+
+export default BlogPost
