c803764f3c1ea87ed1ee85be6a72d27a03e26836
rohit1101 <rohit@Rohits-MacBook-Air.local>
2022-04-30 13:15:20 +0530

    add header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,6 +3,37 @@ import PropTypes from 'prop-types';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import { graphql, useStaticQuery } from 'gatsby';
+import { createGlobalStyle } from 'styled-components/macro';
+
+const GlobalStyles = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Cardo:ital,wght@0,400;0,700;1,400&family=Ubuntu:wght@300;400;500;700&display=swap');
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 16px;
+    height: 100%;
+  }
+  #root {
+    height: 100%;
+  }
+  body {
+  height: 100%;
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+  font-family: 'Cardo', serif;
+  font-family: 'Ubuntu', sans-serif;
+}
+* {
+  margin: 0;
+}
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+`;
 
 function Layout({ children }) {
   const data = useStaticQuery(graphql`
@@ -17,13 +48,16 @@ function Layout({ children }) {
     }
   `);
   return (
-    <div>
+    <>
+      <GlobalStyles />
       <title>{data.site.siteMetadata.title}</title>
-      <h1>Welcome to {data.site.siteMetadata.title}</h1>
-      <Navbar />
+
+      <Navbar>
+        <h1>Welcome to {data.site.siteMetadata.title}</h1>
+      </Navbar>
       {children}
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,28 +3,44 @@ import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 
-const NavList=styled.ul`
-  display:flex;
-  list-style:none;
-  gap: 4px;
-  
-`
+const Nav = styled.nav`
+  max-width: 70%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-function Navbar(props) {
+  ul {
+    list-style-type: none;
+    display: flex;
+    gap: 0 8px;
+  }
+`;
+
+const Header = styled.header`
+  background-color: hsl(44, 92%, 71%);
+`;
+
+function Navbar({ children }) {
   return (
-    <nav>
-      <NavList>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>
-        </li>
-      </NavList>
-    </nav>
+    <Header>
+      <Nav>
+        {children}
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blog/">Blog</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </Nav>
+      <a>github</a>
+      <a>linkedin</a>
+    </Header>
   );
 }
 
